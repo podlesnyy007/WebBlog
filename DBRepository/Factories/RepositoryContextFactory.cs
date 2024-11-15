f0878ecea30db6d2592930dde3dd2ddb7e354d1d
mATVEY <114169264+podlesnyy007@users.noreply.github.com>
2024-11-15 22:40:10 +0400

    Create RepositoryContextFactory.cs

---
+++ DBRepository/Factories/RepositoryContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using DBRepository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace DBRepository
+{
+	public class RepositoryContextFactory : IRepositoryContextFactory
+	{
+		public RepositoryContext CreateDbContext(string connectionString)
+		{
+			var optionsBuilder = new DbContextOptionsBuilder<RepositoryContext>();
+			optionsBuilder.UseSqlServer(connectionString);
+
+			return new RepositoryContext(optionsBuilder.Options);
+		}
+	}
+}
